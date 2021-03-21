ec67b3a9e26a0a53befdf1920b3b94d8b973f2ca
Alessandro Ferraro <ale_ferraro@libero.it>
2021-03-21 15:41:32 +0100

    aggiunge consegna

--- js/main.js
+++ js/main.js
@@ -1,7 +1,14 @@
 /*
+Esercizio di oggi: Simon Says
+repo/cartella js-simon
 Un alert espone 5 numeri casuali diversi.
 Dopo 30 secondi l'utente deve inserire, un prompt alla volta, i numeri che ha visto precedentemente.
 Una volta inseriti i 5 numeri, il software dice quanti e quali numeri sono stati ricordati.
+Consigli del giorno:
+* Pensate prima in italiano.
+* Dividete in piccoli problemi la consegna.
+* Individuate gli elementi di cui avete bisogno per realizzae il programma.
+Buon lavoro
 */
 
 
